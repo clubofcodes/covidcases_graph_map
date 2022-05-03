ce4fa4e2267b1659a404a8c6ee59f8a9bb5c81c3
Rahul Jagetia <rahulj.inexture@gmail.com>
2022-05-03 15:26:32 +0530

    Fix: X-Axis lable alignment.

--- src/screens/Line Graph/CasesGraph.jsx
+++ src/screens/Line Graph/CasesGraph.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend, Scatter, Area } from 'recharts';
+import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip, ResponsiveContainer, Legend } from 'recharts';
 //dummy data for line chart.
 // const data = [{ name: 'Page A', uv: 400, pv: 2400, amt: 2400 }, { name: 'Page A', uv: 100, pv: 2400, amt: 2400 }, { name: 'Page A', uv: 460, pv: 2400, amt: 2400 }];
 
@@ -28,10 +28,10 @@ const CasesGraph = () => {
         apiError ? <div> {apiError} </div> :
             casesData.length ?
                 <ResponsiveContainer width="100%" aspect={2.5}>
-                    <LineChart data={casesData.slice(0, 15)} width={500} height={300} margin={{ top: 5, right: 30, bottom: 5, left: 40 }} >
+                    <LineChart data={casesData.slice(0, 20)} width={500} height={300} margin={{ top: 5, right: 30, bottom: 5, left: 40 }} >
                         <CartesianGrid stroke="white" strokeDasharray="3 3" />
                         <Line type="monotone" dataKey="cases" stroke="red" dot={{ r: 5 }} activeDot={{ stroke: 'white', strokeWidth: 2, r: 8 }} strokeWidth={2} />
-                        <XAxis dataKey="country" interval={'preserveStartEnd'} minTickGap={0} tickSize={15} allowDataOverflow={true} />
+                        <XAxis dataKey="country" interval={0} minTickGap={0} tickSize={15} allowDataOverflow={true} angle='300' />
                         <YAxis />
                         <Legend />
                         <Tooltip contentStyle={{ backgroundColor: 'white', color: 'black' }} />
